489ed53df34671fc7013624771a169b578da4ac5
EssenceOfChaos <fjschiller@gmail.com>
2018-03-29 14:58:45 -0400

    initialize git repo

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flappy Bird in ReactJS
+
+* React v16.2.0
+* Bootstraped with create-react-app
+
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GridCell from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<GridCell />, document.getElementById('root'));
+registerServiceWorker();
